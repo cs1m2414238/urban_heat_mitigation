36c7baf4e47c036e9581d646eea6d390d657ad0a
Priyanshu Sharma <2k24.cs1m.2414238@gmail.com>
2026-06-29 01:24:34 +0530

    components
    Signed-off-by: Priyanshu Sharma <2k24.cs1m.2414238@gmail.com>

---
+++ frontend/src/components/SidebarControls.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SidebarControls: React.FC = () => {
+  return (
+    <div className="sidebar-controls">
+      <h3>Controls Panel Placeholder</h3>
+    </div>
+  );
+};
